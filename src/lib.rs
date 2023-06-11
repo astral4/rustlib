1c78a16061eebbf11a91b648aecd4d4512dedfce
astral4 <88992929+astral4@users.noreply.github.com>
2023-06-10 22:55:59 -0400

    reduce annoyingness by setting "unused" lint group to warn

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! {{project-description}}
 
-#![warn(clippy::pedantic, future_incompatible)]
+#![warn(clippy::pedantic, future_incompatible, unused)]
 #![deny(
     let_underscore_drop,
     macro_use_extern_crate,
@@ -18,7 +18,6 @@
     trivial_numeric_casts,
     unreachable_pub,
     unsafe_op_in_unsafe_fn,
-    unused,
     unused_crate_dependencies,
     unused_import_braces,
     unused_lifetimes,
